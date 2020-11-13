4a3c33e7706c4e4bd5e5caed7470babea8247de6
Chien81 <cheyennehunt78@gmail.com>
2020-11-12 18:26:54 -0800

    Updates

---
+++ precache-manifest.59ee2b24719015e3c73ce254901ae136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da625fe9f5baff6ca359955ed2d7671",
+    "url": "/edirect/index.html"
+  },
+  {
+    "revision": "4ddf7aee60d2a9a7a71b",
+    "url": "/edirect/static/css/main.8a94b82e.chunk.css"
+  },
+  {
+    "revision": "eb77333a937eba9ee4c7",
+    "url": "/edirect/static/js/2.0635a69d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/edirect/static/js/2.0635a69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddf7aee60d2a9a7a71b",
+    "url": "/edirect/static/js/main.a3faea14.chunk.js"
+  },
+  {
+    "revision": "78cdc8a0273cf7f2ac70",
+    "url": "/edirect/static/js/runtime-main.13019b75.js"
+  }
+]);
